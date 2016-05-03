bdda9feb61261b330eb539c79ae98ae9f61c6f71
sensei100 <jenanton01@gmail.com>
2016-05-03 06:06:10 +0300

    Done.

--- looping.rb
+++ looping.rb
@@ -1,5 +1,11 @@
 def looping
-	#your code here
+  counter = 0
+  counter +=1
+  levitation_charm = "Wingardium Leviosa"
+	loop do |counter|
+    puts levitation_charm
+  end
 end
 
 #call your method here
+looping
